b39bd9c3a2cab8d6476d7bb756ae015570221120
Ansh Kumar Singh <anshkr.win8@gmail.com>
2026-05-18 14:57:46 +0530

    Practice Question 9 Completed

--- 18_Practice_Question_9.js
+++ 18_Practice_Question_9.js
@@ -2,3 +2,16 @@
 //  Append "From Apna College Students" to this text using JS.
 
 // Create 3 divs with common class name - "box". Access them and add some unique text to each of them.  
+
+const heading = document.createElement('h2');
+heading.textContent = 'Hello JavaScript';
+heading.textContent += ' From Apna College Students';
+heading.id = 'head1';
+document.body.appendChild(heading);
+
+for (let i = 1; i <= 3; i++) {
+  const box = document.createElement('div');
+  box.className = 'box';
+  box.textContent = `Box ${i}: This is unique text for box ${i}.`;
+  document.body.appendChild(box);
+}
